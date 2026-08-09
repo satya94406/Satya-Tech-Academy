19a8ab85c44fc50c62ad81af5e0123d8db98e918
Satya1534 <satyaprakash@web3task.com>
2026-08-09 22:22:48 +0530

    3 baar ui resolve

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -95,7 +95,7 @@ export default function Navbar() {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth={2}
-                d="M4 6h16M4 12h16m-7 6h7"
+                d="M4 6h16M4 12h16M4 18h16"
               />
             )}
           </svg>
